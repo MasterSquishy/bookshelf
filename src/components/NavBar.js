3972303a917641cd19a699a73eac43ef2fb43c65
Chanelle Green <chanelle.green@gmail.com>
2022-11-19 16:59:38 -0500

    Use Routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 
 const NavBar = ({ appName, pages, query, onSearchQuery }) => {
   let location = useLocation();
-  console.log("location", location.pathname);
+
   return (
     <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
       <div className="container-fluid">
@@ -14,30 +14,28 @@ const NavBar = ({ appName, pages, query, onSearchQuery }) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {pages.map((p) => (
-              <>
-                <li key={p.name} className="nav-item">
-                  <NavLink
-                    to={p.path}
-                    className={(isActive) =>
-                      "nav-link" + (!isActive ? " unselected" : "")
-                    }
-                    aria-current="page"
-                  >
-                    {p.name}
-                  </NavLink>
-                </li>
-                {p.search && location.pathname === "/search" && (
-                  <div className="searchBar">
-                    <SearchBar
-                      placeholder="Title, author, or ISBN..."
-                      onSearchQuery={onSearchQuery}
-                      query={query}
-                    />
-                  </div>
-                )}
-              </>
+              <li key={p.name} className="nav-item">
+                <NavLink
+                  to={p.path}
+                  className={(isActive) =>
+                    "nav-link" + (!isActive ? " unselected" : "")
+                  }
+                  aria-current="page"
+                >
+                  {p.name}
+                </NavLink>
+              </li>
             ))}
           </ul>
+          {location.pathname === "/search" && (
+            <div className="searchBar">
+              <SearchBar
+                placeholder="Title, author, or ISBN..."
+                onSearchQuery={onSearchQuery}
+                query={query}
+              />
+            </div>
+          )}
         </div>
       </div>
     </nav>
