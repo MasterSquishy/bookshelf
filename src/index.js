6d1e63e14aa4bd9fb436ca47c00d9f116978a95a
Chanelle Green <chanelle.green@gmail.com>
2022-11-19 16:58:09 -0500

    refactor component tree

--- src/index.js
+++ src/index.js
@@ -3,35 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import App from "./components/App";
-import NavBar from "./components/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-let query = "React";
-const navPages = [
-  { name: "Home", path: "/", search: false },
-  {
-    name: "Search",
-    path: "/search",
-    active: false,
-    search: true,
-    collection: [],
-  },
-];
-
-const handleSearchQuery = (event) => {
-  event.preventDefault();
-  query = event.target.value;
-  console.log(event.target.value);
-};
 
 root.render(
   <BrowserRouter>
-    <NavBar
-      appName="Bookshelf"
-      pages={navPages}
-      onSearchQuery={handleSearchQuery}
-      query={query}
-    />
-    <App query={query} />
+    <App />
   </BrowserRouter>
 );
