24629a3b018d2d9ddb4a2218167857f5b7e7368a
Chanelle Green <chanelle.green@gmail.com>
2022-11-19 16:58:58 -0500

    refactor book updates

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,36 @@ import _ from "lodash";
 import "../css/App.css";
 import MyBooks from "./MyBooks";
 import Search from "./Search";
+import NavBar from "./NavBar";
 import * as BooksAPI from "../utils/BooksAPI";
 
-function App({ query }) {
+function App() {
   const [myBooks, setMyBooks] = useState([]); // Books that belong to one of my collections
   const [searchBooks, setSearchBooks] = useState([]); //books returned from API search query
+  const [query, setQuery] = useState("");
+  const navPages = [
+    { name: "Home", path: "/", search: false },
+    {
+      name: "Search",
+      path: "/search",
+      active: false,
+      search: true,
+      collection: [],
+    },
+  ];
 
-  const [shelves, setShelves] = useState([
-    {
-      name: "Currently Reading",
-      id: "currentlyReading",
-      collection: [],
-    },
-    {
-      name: "Want to Read",
-      id: "wantToRead",
-      collection: [],
-    },
-    {
-      name: "Read",
-      id: "read",
-      collection: [],
-    },
-  ]);
+  const [shelves, setShelves] = useState({
+    currentlyReading: ["1"],
+    wantToRead: ["1"],
+    read: ["1"],
+  });
+
+  const updateBook = async (book) => {
+    const res = await BooksAPI.update(book, book.shelf);
+
+    console.log("The Problem", res);
+    return res;
+  };
 
   useEffect(() => {
     // Get All Books from the api
@@ -42,73 +49,53 @@ function App({ query }) {
     getMyBooks();
   }, []);
 
-  useEffect(() => {
-    let tempShelves = new Set();
-    // Ensure that these are the first 3 shelves in the list
-    tempShelves.add("currentlyReading");
-    tempShelves.add("wantToRead");
-    tempShelves.add("read");
-
-    // Extract unique shelves from booklist
-    myBooks.forEach((b) => {
-      tempShelves.add(b.shelf);
-    });
-
-    // Add an id and collection of books to each shelf
-    tempShelves = Array.from(tempShelves).map((s) => ({
-      name: _.startCase(s),
-      id: s,
-      collection: myBooks.filter((b) =>
-        b.shelf === "" ? b.shelf === "none" : b.shelf === s
-      ),
-    }));
-
-    setShelves(tempShelves);
-  }, [myBooks]);
-
   const handleShelfChange = (bookId, newShelf) => {
     let target = myBooks.find((book) => book.id === bookId);
 
     target.shelf = newShelf;
 
-    // update booklist with new shelf for target book
-    setMyBooks(
-      myBooks.map((b) => (b.id === bookId ? { ...b, shelf: newShelf } : b))
-    );
+    updateBook(target, newShelf).then(setShelves);
   };
 
-  const handleAddShelf = (name) => {
-    const newShelfObject = {
-      name: name,
-      id: _.camelCase(name),
-      collection: [],
-    };
-    setShelves([...shelves, newShelfObject]);
+  const handleSearchQuery = (event) => {
+    event.preventDefault();
+    setQuery(event.target.value);
   };
+
   return (
-    <Routes>
-      <Route
-        exact
-        path="/"
-        element={
-          <MyBooks
-            shelves={shelves}
-            onAddShelf={handleAddShelf}
-            onShelfChange={handleShelfChange}
-          />
-        }
+    <>
+      <NavBar
+        appName="Bookshelf"
+        pages={navPages}
+        onSearchQuery={handleSearchQuery}
+        query={query}
       />
-      <Route
-        path="/search"
-        element={
-          <Search
-            shelves={shelves}
-            onShelfChange={handleShelfChange}
-            query={query}
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <MyBooks
+                shelves={shelves}
+                onShelfChange={handleShelfChange}
+                myBooks={myBooks}
+              />
+            }
           />
-        }
-      />
-    </Routes>
+          <Route
+            path="/search"
+            element={
+              <Search
+                shelves={shelves}
+                onShelfChange={handleShelfChange}
+                query={query}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- src/components/BookCollection.js
+++ src/components/BookCollection.js
@@ -6,39 +6,46 @@ const BookCollection = ({ name, bookList, shelves, onShelfChange }) => {
   const getBookData = async (bookId) => {
     const res = await BooksAPI.get();
     const book = res;
+
     return book.shelf;
   };
   return (
     <div className="bookshelf">
       <div className="bookshelf-title">{name}</div>
-      <div className="books-grid">
-        {bookList.map((b) => (
-          <li className="book" key={b.id}>
-            <div>
-              <div
-                className="book-cover"
-                style={{
-                  backgroundImage: `url(${b.imageLinks.thumbnail})`,
-                }}
-              ></div>
-              <DropUp
-                header="Move to..."
-                options={shelves}
-                onSelect={onShelfChange}
-                bookId={b.id}
-                currentShelf={getBookData(b.id)}
-              />
-            </div>
-            <p className="book-title">{b.title}</p>
-            {b.authors.map((author, i) => (
-              <span className="book-authors" key={i}>
-                {i > 0 && ", "}
-                {author}
-              </span>
-            ))}
-          </li>
-        ))}
-      </div>
+      {bookList && (
+        <div className="books-grid">
+          {bookList.map((b) => (
+            <li key={b.id}>
+              <div className="book">
+                <div
+                  className="book-cover"
+                  style={
+                    b.imageLinks && {
+                      backgroundImage: `url(${b.imageLinks.thumbnail})`,
+                    }
+                  }
+                ></div>
+                <DropUp
+                  header="Move to..."
+                  options={shelves}
+                  onSelect={onShelfChange}
+                  bookId={b.id}
+                  currentShelf={b.shelf}
+                />
+
+                <p className="book-title">{b.title}</p>
+                {b.authors &&
+                  b.authors.map((author, i) => (
+                    <span className="book-authors" key={i}>
+                      {i > 0 && ", "}
+                      {author}
+                    </span>
+                  ))}
+              </div>
+            </li>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
